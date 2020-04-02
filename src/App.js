133dad5d7eab68a69282a115a9ac43de74ab8aa6
Lockett Pundt <kalifornium@hotmail.com>
2020-04-02 13:12:36 -0400

    added the routes to the app.js that lead to the compnents created so far.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Styled from 'styled-components';
 import axios from 'axios';
+import Recipes from './components/Recipes';
+import LogIn from './components/LogIn';
+
 
 const App = () => {
   const [testInfo, setTestInfo ] = useState([]);
   
   useEffect(() => {
     const fetchData = async () => {
-      const response = await axios.get('http://localhost:4000');
+      const response = await axios.get('http://localhost:4000/');
       setTestInfo(response.data);
     };
     fetchData();
@@ -19,7 +22,15 @@ const App = () => {
   
   return (
     <div className="App">
-      <h1>here we go!</h1>
+      <Router>
+        <Route path='/' exact>
+          <h1>{testInfo.title}here we go!</h1>
+          <a href="/user/login">Log In</a>
+          <a href="/recipes">Recipe list</a>
+        </Route>
+        <Route path='/recipes' component={Recipes} />
+        <Route path='/user/login' component={LogIn} />
+      </Router>
     </div>
   );
 }
