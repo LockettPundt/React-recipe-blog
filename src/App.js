a01cad7675dce2eb2b0eeee7b29535f84650b9fb
Lockett Pundt <kalifornium@hotmail.com>
2020-04-02 11:08:45 -0400

    initialized state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Styled from 'styled-components';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [testInfo, setTestInfo ] = useState([]);
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get('http://localhost:4000');
+      setTestInfo(response.data);
+    };
+    fetchData();
+  }, []);
+  
+  console.log(testInfo);
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>here we go!</h1>
     </div>
   );
 }
