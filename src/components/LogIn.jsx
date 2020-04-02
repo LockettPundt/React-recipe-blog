7901f43bdc557c1b4224b71f3bd583f5785e4b05
Lockett Pundt <kalifornium@hotmail.com>
2020-04-02 13:11:42 -0400

    log in component skeleton.

---
+++ src/components/LogIn.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+
+
+const LogIn = () => {
+  
+  return (
+    <form action="/users/login" method="POST" class="forms">
+      <label>Email:
+          <input type="text" name="user_email" placeholder="Enter Your Email" />
+      </label>
+      <label>Password:
+          <input type="password" name="user_password" placeholder="Enter Your Password" />
+      </label >
+      <button type="submit">LOG IN</button>
+    </form>
+  )
+}
+
+
+export default LogIn;
