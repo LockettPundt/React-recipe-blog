8c9b85be267ad5fcd53eafedd39c853acbdff770
Lockett Pundt <kalifornium@hotmail.com>
2020-04-03 12:15:24 -0400

    added the same styled components to the recipe submission form.

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -4,18 +4,59 @@ import styled from 'styled-components';
 
 
 const Thumbnail = styled.img`
-    width: 10vh;
-    height: 10vh;
-    border-radius: 6px;
-    margin: 0 2vh;
-  `;
+  width: 10vh;
+  height: 10vh;
+  border-radius: 6px;
+  margin: 0 2vh;
+`;
   
-  const RecipeItem = styled.div`
-    display: flex;
-    align-items: center;
-    padding: 1vw;
-    
-  `
+const RecipeItem = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 1vw;
+  
+`
+const Form = styled.form`
+  font-size: 2.6vh;
+  background-color: #ffd9de;
+  background-image: linear-gradient(315deg, #ffd9de 0%, #e99ba6 74%);
+  color: black;
+  padding: 2vh;
+  border-radius: 6px;
+  display: flex;
+  flex-direction: column;
+
+`
+const Input = styled.input`
+  border-radius: 3px;
+  width: 30%;
+  border: none;
+  padding: 1vh;
+  
+`
+const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+`
+const TextArea = styled.textarea`
+  border-radius: 3px;
+  width: 30%;
+  border: none;
+  padding: 1vh;
+`
+const Button = styled.button`
+  font-size: 2vw;
+  margin: 2vw 0vh;;
+  width: 32%;
+  border-radius: 3px;
+  color: black;
+  border: 5px solid black;
+  padding: 2vh;
+  
+  &hover {
+    background-color: red;
+  }
+`
 
 const Recipes = () => {
   
@@ -52,28 +93,28 @@ const Recipes = () => {
       <ul>
         {recipeComponents}
       </ul>
-      <form  action="recipelist"  method="POST" autoComplete="off">
+      <Form  action="recipelist"  method="POST" autoComplete="off">
         <h2>Submit A Recipe:</h2>
-        <input type="hidden" name="user_id" value=""/>
-        <input type="hidden" name="first_name" value=""/>
-        <input type="hidden" name="last_name" value=""/>
-        <label>Recipe Title:
-            <input type="text" name="title" placeholder="Recipe Title"/>
-        </label>
-        <label>Rating: 
-            <input type="text" name="rating" placeholder="Rate This Recipe"/>
-        </label>
-        <label>Ingredients: 
-            <textarea name="ingredients" placeholder="Seperated by commas please ;)" rows="10" cols="40"></textarea>
-        </label>
-        <label>Directions: 
-            <textarea name="directions" placeholder="Enter Your Directions" rows="10" cols="40"></textarea>
-        </label>
-        <label>Image: 
-            <input type="file" name="img_upload"/>
-        </label>
-        <button type="submit">Submit Recipe</button>
-    </form>
+        <Input type="hidden" name="user_id" value=""/>
+        <Input type="hidden" name="first_name" value=""/>
+        <Input type="hidden" name="last_name" value=""/>
+        <Label>Recipe Title:
+            <Input type="text" name="title" placeholder="Recipe Title"/>
+        </Label>
+        <Label>Rating: 
+            <Input type="text" name="rating" placeholder="Rate This Recipe"/>
+        </Label>
+        <Label>Ingredients: 
+            <TextArea name="ingredients" placeholder="Seperated by commas please ;)" rows="10" cols="40"></TextArea>
+        </Label>
+        <Label>Directions: 
+            <TextArea name="directions" placeholder="Enter Your Directions" rows="10" cols="40"></TextArea>
+        </Label>
+        <Label>Image: 
+            <Input type="file" name="img_upload"/>
+        </Label>
+        <Button type="submit">Submit Recipe</Button>
+    </Form>
       <a href='/'>Home</a>
     </section>
   );
